d59daa69fb598a06bf2ef4befbf885923dbc2776
DeveloperCraig <40456881+DeveloperCraig@users.noreply.github.com>
2021-05-06 17:51:52 +0100

    + Added the classes + fixed the NameSpace issue for the IWordFrequency interface

---
+++ BlueRockingWordCount/Classes/WordFrequency.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueRockingWordCount.Classes
+{
+    public class WordFrequency : BlueRockingWordCount.Interfaces.IWordFrequency
+    {
+        public string Word { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Frequency { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ BlueRockingWordCount/Classes/WordFrequencyAnalyzer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueRockingWordCount.Classes
+{
+    public class WordFrequencyAnalyzer : BlueRockingWordCount.Interfaces.IWordFrequencyAnalyzer
+    {
+        public int CalculateFrequencyForWord(string text, string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CalculateHighestFrequency(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<BlueRockingWordCount.IWordFrequency> CalculateMostFrequentWords(string text, int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
